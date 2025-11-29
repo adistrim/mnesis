ebc85403fe9b363840e5aff9e6c168fd28f6a3d3
adistrim <adistrim.dev@zohomail.in>
2025-11-29 20:56:01 +0530

    prepare v1Routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,16 @@
-import { getMessageSchema } from "./v1/message";
+import { requireMethod } from "@/middleware/methodChecker";
+import { withPrefix } from "./utils";
+import { chatRoutes } from "./v1/chat";
+import { messageRoutes } from "./v1/message";
 
-export const routes = {
-    "/v1/messages": getMessageSchema,
-};
+function rootRoute() {
+    return new Response("Welcome to the API v1", {
+        headers: { "Content-Type": "text/plain" },
+    });
+}
+
+export const v1Routes = withPrefix("/api/v1", {
+    "/": requireMethod("GET", rootRoute),
+    ...chatRoutes,
+    ...messageRoutes,
+});
