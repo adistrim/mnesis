1bab460d66d42f536a946c67b5566a3c2fdd5026
adistrim <adistrim.dev@zohomail.in>
2025-12-05 10:23:03 +0530

    init ui components lib: - tailwindcss - shadcn/ui - caffeine from tweakcn

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <h1>mnesis</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+      </div>
   </StrictMode>,
 )
