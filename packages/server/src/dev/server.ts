f84ca8c6d5a8c5644094bb22490f1ca1ddcfb894
adistrim <adistrim.dev@zohomail.in>
2025-12-13 15:34:13 +0530

    serverless config

---
+++ packages/server/src/dev/server.ts
@@ -0,0 +1,27 @@
+import { settings } from "@/config/settings";
+import { v1Routes } from "@/routes";
+import { serve } from "bun";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono()
+
+app.use("*", cors({
+    origin: settings.CORS_ALLOWED_ORIGINS.length > 0
+        ? settings.CORS_ALLOWED_ORIGINS
+        : [],
+}));
+
+app.route("/api/v1", v1Routes);
+
+serve({
+    port: settings.PORT,
+    fetch: app.fetch,
+});
+
+console.log(`
+    Server running
+    URL: ${settings.HOST}:${settings.PORT}
+    CORS Allowed Origins: ${settings.CORS_ALLOWED_ORIGINS.join(", ") || "none"}
+    Press Ctrl+C to stop.
+`);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,27 +1,27 @@
+/*
+
+    FOR SERVERLESS DEPLOYMENT ONLY
+
+*/
+
+
 import { settings } from "@/config/settings";
-import { v1Routes } from "./routes";
-import { serve } from "bun";
+import { v1Routes } from "@/routes";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
 app.use("*", cors({
-    origin: settings.CORS_ALLOWED_ORIGINS.length > 0
-        ? settings.CORS_ALLOWED_ORIGINS
-        : [],
+  origin: settings.CORS_ALLOWED_ORIGINS.length > 0
+    ? settings.CORS_ALLOWED_ORIGINS
+    : [],
 }));
 
 app.route("/api/v1", v1Routes);
 
-serve({
-    port: settings.PORT,
-    fetch: app.fetch,
+app.get("/", ctx => {
+    return ctx.text("I am alive!");
 });
 
-console.log(`
-    Server running
-    URL: ${settings.HOST}:${settings.PORT}
-    CORS Allowed Origins: ${settings.CORS_ALLOWED_ORIGINS.join(", ") || "none"}
-    Press Ctrl+C to stop.
-`);
+export default app;
