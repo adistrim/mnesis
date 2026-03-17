6a5898220fcf0be37d4071795fc85aa8b323b4e4
adistrim <adistrim.dev@zohomail.in>
2026-03-17 08:22:52 +0530

    Update server dependencies and refactor app initialization

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,19 @@
-import { handle } from "hono/vercel";
-import { createApp } from "./app";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { settings } from "./config/settings";
+import { v1Routes } from "./routes";
+import { errorResponse } from "./utils/errorResponse";
+import { resourceNotFoundError } from "./lib/errors";
 
-const app = createApp();
+const app = new Hono();
 
-export default handle(app);
+app.use("/*", cors({ origin: settings.CORS_ALLOWED_ORIGINS }));
+
+app.route("/api/v1", v1Routes);
+
+app.notFound(() => errorResponse(resourceNotFoundError()));
+
+app.onError((err) => errorResponse(err));
+
+export default app;
+
